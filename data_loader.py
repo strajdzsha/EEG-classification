c7a3aee4d7bdcf3b7861dc6742cab5b9b02d703b
PinkFrojdSenjak <pavle.padjin@gmail.com>
2023-05-11 16:01:01 +0200

    change convention to slash

--- data_loader.py
+++ data_loader.py
@@ -56,7 +56,7 @@ class DataLoader:
         else:
             with open(self.filepaths[self.idx], 'rb') as f:
                 data = pickle.load(f)
-                folder_name = self.filepaths[self.idx].split('\\')[-2]
+                folder_name = self.filepaths[self.idx].split('/')[-2]
                 gender, age, group, mmse = folder_name.split('-')[2:]
                 self.idx += 1
                 return {
